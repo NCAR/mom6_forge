bfd0273f7d49035c728aa1c328c29c2705543c70
manishvenu <manish.venumuddula@gmail.com>
2026-04-17 13:05:08 -0600

    Changes

--- mom6_forge/topo.py
+++ mom6_forge/topo.py
@@ -1387,9 +1387,8 @@ class Topo:
             },
         )
 
-        # tmask is calculated from the depth, which is calculated from the unmasked_depth + mask, so we're preserving the mask here! Don't worry!
         ds["mask"] = xr.DataArray(
-            self.tmask.astype(np.int32),
+            self.mask.astype(np.int32),
             dims=["ny", "nx"],
             attrs={
                 "long_name": "landsea mask at t points: 1 ocean, 0 land",
