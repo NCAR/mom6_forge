5aa4dc6df969a98a15b94f534d55d41f1644322d
manishvenu <manish.venumuddula@gmail.com>
2026-06-29 16:31:35 -0600

    Change defaults

--- tests/conftest.py
+++ tests/conftest.py
@@ -292,13 +292,13 @@ def get_rect_grid():
 
 @pytest.fixture
 def get_rect_topo(get_rect_grid, tmp_path):
-    topo = Topo(get_rect_grid, min_depth=0, version_control_dir=tmp_path)
+    topo = Topo(get_rect_grid, min_depth=0, version_control_dir=tmp_path, git = True)
     topo.set_flat(1000)
     return topo
 
 
 @pytest.fixture
-def get_rect_topo_without_vc(get_rect_grid, tmp_path):
+def get_rect_topo_without_vc(get_rect_grid):
     topo = Topo(get_rect_grid, min_depth=0, git=False)
     topo.set_flat(1000)
     return topo
