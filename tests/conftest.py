567407cfc452724aa91d2e058ac5c4d54102c8df
manishvenu <manish.venumuddula@gmail.com>
2026-06-29 16:49:31 -0600

    FileTestFixtureName

--- tests/conftest.py
+++ tests/conftest.py
@@ -298,7 +298,7 @@ def get_rect_topo(get_rect_grid, tmp_path):
 
 
 @pytest.fixture
-def get_rect_topo(get_rect_grid, tmp_path):
+def get_rect_topo_with_vc(get_rect_grid, tmp_path):
     topo = Topo(get_rect_grid, min_depth=0, version_control_dir=tmp_path, git=True)
     topo.set_flat(1000)
     return topo
