c913469beccdb8a63dd07acb4ccd30a9e6168526
manishvenu <manish.venumuddula@gmail.com>
2026-09-03 16:19:08 -0600

    Make Fancy Regridding Tests Slow

--- tests/test_mapping.py
+++ tests/test_mapping.py
@@ -283,7 +283,7 @@ def test_smoke_seams_and_global_make_subgrid_points(
         grid.qlon.values, grid.qlat.values, nx_sub, ny_sub
     )
 
-
+@pytest.mark.slow
 def test_regrid_with_subsampling(get_simple_grid):
     # Test with a simple 2x2 grid and 2 sub-points per cell with data that lands exactly on the sub points (subtracted by 0.1 to show snapping to sub points)
     nx_sub = ny_sub = 2
@@ -318,7 +318,7 @@ def test_regrid_with_subsampling(get_simple_grid):
         ds["data"].values, expected_data
     ), "Regridded data does not match expected values."
 
-
+@pytest.mark.slow
 def test_regrid_with_subsampling_time_dim(get_simple_grid):
     nx_sub = ny_sub = 2
     grid = get_simple_grid
